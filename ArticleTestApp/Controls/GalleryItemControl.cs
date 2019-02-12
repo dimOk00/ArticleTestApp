7969621511a876ca5ddf97d64c3644561a674e65
dim_ok00 <sudoma13@gmail.com>
2019-02-12 15:43:07 +0200

    Add templated controls

---
+++ ArticleTestApp/Controls/GalleryItemControl.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using ArticleLib.Data;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ArticleTestApp.Controls
+{
+    public sealed class GalleryItemControl : Control
+    {
+        private ImageBrush MainImage { get; set; }
+
+        public string NameOfImage
+        {
+            get => (string)GetValue(NameOfImageProperty);
+            set => SetValue(NameOfImageProperty, value);
+        }
+
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        public Uri UriSource
+        {
+            get => (Uri)GetValue(UriSourceProperty);
+            set => SetValue(UriSourceProperty, value);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            MainImage = GetTemplateChild("MainImage") as ImageBrush;
+        }
+
+        public static readonly DependencyProperty NameOfImageProperty =
+            DependencyProperty.Register("NameOfImage", typeof(string), typeof(GalleryItemControl), new PropertyMetadata(null, null));
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(GalleryItemControl), new PropertyMetadata(null, null));
+
+        public static readonly DependencyProperty UriSourceProperty =
+            DependencyProperty.Register("UriSource", typeof(Uri), typeof(GalleryItemControl), new PropertyMetadata(null, OnImageChange));
+
+        private static void OnImageChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var img = (d as GalleryItemControl)?.MainImage;
+            if (img != null)
+            {
+                img.ImageSource = new BitmapImage(e.NewValue as Uri);
+            }
+        }
+
+        public GalleryItemControl()
+        {
+            DefaultStyleKey = typeof(GalleryItemControl);
+        }
+    }
+}
---
+++ ArticleTestApp/Controls/SubTitleControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using ArticleLib.Data;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ArticleTestApp.Controls
+{
+    public sealed class SubTitleControl : Control
+    {      
+        public static readonly DependencyProperty FirstNameProperty =
+            DependencyProperty.Register("FirstName", typeof(string), typeof(SubTitleControl), new PropertyMetadata(null, null));
+
+        public static readonly DependencyProperty LastNameProperty =
+            DependencyProperty.Register("LastName", typeof(string), typeof(SubTitleControl), new PropertyMetadata(null, null));
+
+        public static readonly DependencyProperty DateProperty =
+            DependencyProperty.Register("Date", typeof(string), typeof(SubTitleControl), new PropertyMetadata(null, null));
+        
+        public string FirstName
+        {
+            get => (string)GetValue(FirstNameProperty);
+            set => SetValue(FirstNameProperty, value);
+        }
+
+        public string LastName
+        {
+            get => (string)GetValue(LastNameProperty);
+            set => SetValue(LastNameProperty, value);
+        }
+
+        public string Date
+        {
+            get => (string)GetValue(DateProperty);
+            set => SetValue(DateProperty, value);
+        } 
+
+        public SubTitleControl()
+        {
+            this.DefaultStyleKey = typeof(SubTitleControl);
+        }
+    }
+}
